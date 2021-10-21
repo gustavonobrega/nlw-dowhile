1a00b80d434f0d73f285a67c210ff447b70cf8b8
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2021-10-21 18:09:47 -0300

    Message list component

--- frontend/src/components/MessageList/index.tsx
+++ frontend/src/components/MessageList/index.tsx
@@ -1,6 +1,56 @@
+import styles from './styles.module.scss';
+
+import logoImg from '../../assets/logo.svg';
+
 export function MessageList() {
   return (
-    <h1>MessageList</h1>
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+    
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com 
+            certeza vai ser o melhor dde todos os tempos,
+            vamo pra cima!
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage} >
+              <img src="https://github.com/gustavonobrega.png" alt="Gustavo Nobrega" />
+            </div>
+            <span>Gustavo Nobrega</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com 
+            certeza vai ser o melhor dde todos os tempos,
+            vamo pra cima!
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage} >
+              <img src="https://github.com/gustavonobrega.png" alt="Gustavo Nobrega" />
+            </div>
+            <span>Gustavo Nobrega</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com 
+            certeza vai ser o melhor dde todos os tempos,
+            vamo pra cima!
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage} >
+              <img src="https://github.com/gustavonobrega.png" alt="Gustavo Nobrega" />
+            </div>
+            <span>Gustavo Nobrega</span>
+          </div>
+        </li>
+      </ul>
+    </div>
   )
 }
 
